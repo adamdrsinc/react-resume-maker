e91bc2e3bd8aa7dcaf96edb1a632752bb4e7cc45
adamdrsinc <adam.sinc124@gmail.com>
2025-10-05 14:35:32 +0100

    feat: add links and author to footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,7 +1,13 @@
 import "./Footer.css";
+import gitHubPng from "./assets/github.png";
+import linkedInPng from "./assets/linked-in.png";
 
 export default function Footer(){
     return (
-        <footer></footer>
+        <footer>
+            <h1>Made by Adam Sinclair</h1>
+            <a href="https://github.com/adamdrsinc" target="_blank"><img src={gitHubPng} alt="Adam Sinclair's GitHub Profile" /></a>
+            <a href="https://www.linkedin.com/in/adamdrsinclair/" target="_blank"><img src={linkedInPng} alt="Adam Sinclair's LinkedIn Profile" /></a>
+        </footer>
     );
 }
